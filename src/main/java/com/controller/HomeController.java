2477487ee5937bdbacd3dc5c2574087dc19d1247
zouyongchuan <810947215@qq.com>
2020-03-08 17:48:46 +0800

    enhance static sources:htmls and js

--- src/main/java/com/controller/HomeController.java
+++ src/main/java/com/controller/HomeController.java
@@ -1,13 +1,14 @@
 package com.controller;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
-@Controller
+@RestController
 public class HomeController {
     @RequestMapping(value ="/",method = RequestMethod.GET)
-    public String homePage(){
-        return "home";
+    public ModelAndView homePage(){
+        return new ModelAndView("home");
     }
 }
